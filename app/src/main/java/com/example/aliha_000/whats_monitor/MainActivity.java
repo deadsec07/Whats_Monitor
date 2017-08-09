26de89fe803f116b5bb5432c3a1f320c85848756
r3b311i0n <nasashinega@gmail.com>
2017-08-09 07:44:16 +0530

    Experiment a bit (because of no practice)

--- app/src/main/java/com/example/aliha_000/whats_monitor/MainActivity.java
+++ app/src/main/java/com/example/aliha_000/whats_monitor/MainActivity.java
@@ -1,23 +1,26 @@
 package com.example.aliha_000.whats_monitor;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
-
-    private Toolbar mToolbar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +28,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        mToolbar = (Toolbar) findViewById(R.id.nav_action);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.nav_action);
         setSupportActionBar(mToolbar);
 
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        mToggle = new ActionBarDrawerToggle(this , mDrawerLayout , R.string.open, R.string.close);
+        DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
@@ -38,23 +41,23 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_menu);
-        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem){
-                switch (menuItem.getItemId()){
-                    case(R.id.nav_account):
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case (R.id.nav_account):
                         Intent accountActivity = new Intent(getApplicationContext(), MainActivity.class);
                         startActivity(accountActivity);
                         break;
-                    case(R.id.nav_settings):
+                    case (R.id.nav_settings):
                         Intent settingsActivity = new Intent(getApplicationContext(), SecondActivity.class);
                         startActivity(settingsActivity);
                         break;
-                    case(R.id.nav_logout):
+                    case (R.id.nav_logout):
                         Intent logoutActivity = new Intent(getApplicationContext(), ThirdActivity.class);
                         startActivity(logoutActivity);
                         break;
-                    case(R.id.nav_monitor):
+                    case (R.id.nav_monitor):
                         Intent monitorActivity = new Intent(getApplicationContext(), FourthActivity.class);
                         startActivity(monitorActivity);
                         break;
@@ -66,17 +69,31 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        return mToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
 
-       if (mToggle.onOptionsItemSelected(item)){
-
-           return true;
-       }
-
-        return super.onOptionsItemSelected(item);
+    public void onClickStart(View view) {
+        Context main = getApplicationContext();
+        CharSequence sequence = "Started!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(main, sequence, duration);
+        toast.show();
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        sendIntent.setPackage("com.whatsapp");
+        startActivity(sendIntent);
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo info = pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+            Log.w("APP", Arrays.toString(info.receivers));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
